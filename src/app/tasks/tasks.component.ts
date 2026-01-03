65e9bae7e9d9a3621f0b110ecaf61e41fb27598b
AbhishekDesai56 <abhishekdesai56@gmail.com>
2026-01-03 14:15:58 +0530

    Standalone App is migrated to Module

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,14 +1,10 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
-import { CommonModule } from "@angular/common";
-import { NewTaskComponent } from "./new-task/new-task.component";
 import { NewTaskData } from './task/task.model';
 import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent, NewTaskComponent, CommonModule],
+  standalone: false,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
